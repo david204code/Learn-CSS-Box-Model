5ffe91bdadb955f8482349dda18356cbb27f2392
David Wu <david204code@gmail.com>
2021-10-07 21:46:25 +0100

    Controlling the box model, content-box to border box of box-sizing property

--- myscript.js
+++ myscript.js
@@ -1,9 +1,25 @@
-document.body.setAttribute("dir", "ltr");
+const alertBox = document.querySelector('[role="alert"]');
+const diagram = document.querySelector('svg');
+const sizeSwitch = document.querySelector('#size-switcher');
 
-window.show_scrollbars.addEventListener("input", (e) =>
-  window.box_model.setAttribute("scrollbars", e.target.checked)
-);
+const messages = {
+  'content-box': `
+      <p>If the box is <code>600px</code> wide and we have a border width of <code>10px</code>, a margin of <code>20px</code>, and <code>10px</code> of padding, the total width of the box would be <code>640px</code>.</p>
+    `,
+  'border-box': `
+      <p>If the box is <code>600px</code> wide and we have a border width of <code>10px</code>, a margin of <code>20px</code>, and <code>10px</code> of padding, the total width of the box would be <code>600px</code>.</p>
+    `,
+};
 
-window.inline_scrollbars.addEventListener("input", (e) =>
-  window.box_model.setAttribute("inline-scrollbars", e.target.checked)
-);
+const renderMessage = (key = 'border-box') => {
+  alertBox.innerHTML = messages[key];
+};
+
+// Set sizing attribute based on switch
+sizeSwitch.addEventListener('change', () => {
+  let sizing = sizeSwitch.checked ? 'border-box' : 'content-box';
+  diagram.setAttribute('data-size', sizing);
+  renderMessage(sizing);
+});
+
+renderMessage();
